0011d179a6b69dd874ae8201df38fadf9def13a0
rakib38324 <rakib3832@gmail.com>
2023-07-27 17:32:18 +0600

    Setup project

---
+++ src/Router/Router.js
@@ -0,0 +1,19 @@
+import Home from "../Pages/Home/Home";
+import Main from "../Pages/MainLayer/Main/Main";
+
+const { createBrowserRouter } = require("react-router-dom");
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>,
+            },
+        ]
+    }
+]);
+
+export default router;
